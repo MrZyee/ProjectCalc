4ba1db0a8d7765077a8da0fffd32e2ff14efb22a
dasad44 <dasad33_79@o2.pl>
2022-07-18 18:33:39 +0200

    lambda expression

--- Calculator.cs
+++ Calculator.cs
@@ -8,38 +8,11 @@ namespace FirstProject
 {
     internal class Calculator
     {
-        public int Adding(int arument1 ,int argument2)
-        {
-            //Console.ForegroundColor = ConsoleColor.Green;
-            return arument1 + argument2;
-        }
-        public int Subtraction(int arument1, int argument2)
-        {
-            //Console.ForegroundColor = ConsoleColor.Red;
-            return arument1 - argument2;
-        }
-        public double Division(int arument1, int argument2)
-        {
-
-            if (argument2 == 0 || arument1 == 0)
-            {
-                Console.WriteLine("Nie dzielimy przez 0");
-                return 0;
-            }
-            //Console.ForegroundColor = ConsoleColor.Yellow;
-            return (double) arument1 / argument2;
-        }
-        public int Multiplication(int arument1, int argument2)
-        {
-            return arument1 * argument2;
-        }
-
-        public int ToThePower(int arument1, int argument2)
-        {
-            //Console.ForegroundColor = ConsoleColor.DarkGreen;
-            return (int)Math.Pow(arument1,argument2);
-
-        }
+        public int Adding(int arument1, int argument2) => arument1 + argument2;
+        public int Subtraction(int arument1, int argument2) => arument1 - argument2;
+        public double Division(int arument1, int argument2) => (double)arument1 / argument2;
+        public int Multiplication(int arument1, int argument2) => arument1 * argument2;
+        public int ToThePower(int arument1, int argument2) => (int)Math.Pow(arument1, argument2);
     }
 }
 
